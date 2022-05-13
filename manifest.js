e96060c3c52a416732af8d4a829640fbd105ec94
Burns <brian.burns@unbounce.com>
2022-05-13 09:11:46 -0700

    Add js manifest

---
+++ manifest.js
@@ -0,0 +1,16 @@
+export default {
+  appId: '@burns/promoVideoMaker',
+  version: '0.1',
+  name: 'Promo Video Maker',
+  isBeta: true,
+  isSwappable: true,
+  type: 'media',
+  description: 'Embed a Promo.com video',
+  iconUrl: 'https://app-assets-unbounce-com.s3.amazonaws.com/content-smart-builder-apps/promo-video/icon.svg',
+  moreInfoUrl: 'https://documentation.unbounce.com/hc/en-us/articles/5559084331284',
+  categories: ['media'],
+  files: [],
+  externalScripts: [
+    'https://app-assets-unbounce-com.s3.amazonaws.com/content-smart-builder-apps/app-reviews/promo-video-maker/index.js',
+  ],
+};
